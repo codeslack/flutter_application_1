4773d21bf6f3f2b3006205a02fd5e362692e64ee
codeslackm <codeslack@gmail.com>
2022-07-16 01:14:08 +0530

    part-3 -- build logic and finally finished

--- lib/part3/home.dart
+++ lib/part3/home.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +8,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String calcText = "";
+  String dob = "";
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -19,12 +21,29 @@ class _HomePageState extends State<HomePage> {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Container(
-              height: size.height / 15,
-              color: Colors.amber,
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    height: size.height / 15,
+                    child: Text(
+                      calcText,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  )
+                ],
+              ),
             ),
             Container(
-              height: size.height / 1.7,
+              height: size.height / 1.8,
               decoration: const BoxDecoration(
                 color: Color(0xff2A2D36),
                 borderRadius: BorderRadius.only(
@@ -75,7 +94,7 @@ class _HomePageState extends State<HomePage> {
                     blocks("/"),
                     blocks("0"),
                     blocks("."),
-                    blocks("=", Colors.red)
+                    blocks("=", count == 0 ? Colors.red : Colors.blue)
                   ],
                 ),
               ]),
@@ -87,21 +106,48 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget blocks(String text, [Color? color]) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 15, left: 8, right: 8),
-      child: Container(
-        height: 70,
-        width: 70,
-        decoration: const BoxDecoration(
-            color: Color(0xff22252D),
-            borderRadius: BorderRadius.all(Radius.circular(15))),
-        child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-                color: color ?? Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: () {
+        if (text == "AC") {
+          setState(() {
+            calcText = "";
+            count = 0;
+          });
+        } else if (text == "=") {
+          if (count == 0) {
+            setState(() {
+              dob = calcText;
+              count += 1;
+              calcText = "";
+            });
+          } else {
+            setState(() {
+              calcText = dob;
+              count -= 1;
+            });
+          }
+        } else {
+          setState(() {
+            calcText = calcText + text;
+          });
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(top: 15, left: 8, right: 8),
+        child: Container(
+          height: 70,
+          width: 70,
+          decoration: const BoxDecoration(
+              color: Color(0xff22252D),
+              borderRadius: BorderRadius.all(Radius.circular(15))),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                  color: color ?? Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
         ),
       ),
