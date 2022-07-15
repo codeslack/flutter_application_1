e1257660c5f8c3ad6114679d9bacbac74046f0bf
codeslackm <codeslack@gmail.com>
2022-07-16 00:30:29 +0530

    part-3 -- basic-ui

--- lib/part3/home.dart
+++ lib/part3/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,7 +20,7 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Container(
-              height: size.height / 10,
+              height: size.height / 15,
               color: Colors.amber,
             ),
             Container(
@@ -30,8 +32,77 @@ class _HomePageState extends State<HomePage> {
                   topRight: Radius.circular(30),
                 ),
               ),
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    blocks("AC", Colors.green),
+                    blocks("CC", Colors.green),
+                    blocks("%", Colors.green),
+                    blocks("/", Colors.red)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    blocks("1"),
+                    blocks("2"),
+                    blocks("3"),
+                    blocks("X", Colors.red)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    blocks("4"),
+                    blocks("5"),
+                    blocks("6"),
+                    blocks("-", Colors.red)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    blocks("7"),
+                    blocks("8"),
+                    blocks("9"),
+                    blocks("+", Colors.red)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    blocks("/"),
+                    blocks("0"),
+                    blocks("."),
+                    blocks("=", Colors.red)
+                  ],
+                ),
+              ]),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget blocks(String text, [Color? color]) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15, left: 8, right: 8),
+      child: Container(
+        height: 70,
+        width: 70,
+        decoration: const BoxDecoration(
+            color: Color(0xff22252D),
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                color: color ?? Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
